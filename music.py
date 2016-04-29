a5310e2c7d976a27c0416f2eb6217f9f7294be99
Benjamin Gorman <8076bgorman@gmail.com>
2016-04-29 13:27:04 +0100

    update music code to be more accurate and add transpose function

--- music.py
+++ music.py
@@ -2,6 +2,14 @@ import RPi.GPIO as GPIO
 import time
 import datetime
 
+TRANSPOSE_AMOUNT = -12
+SPEED = 240.0
+
+minim = 2.0 * (30000/SPEED)
+crotchet = 1.0 * (30000/SPEED)
+quaver = 0.5 * (30000/SPEED)
+semiquaver = 0.25 * (30000/SPEED)
+
 GPIO.setwarnings(False) #disable runtime warnings
 GPIO.setmode(GPIO.BCM) #use Broadcom GPIO names
 GPIO.setup(10, GPIO.OUT) #set pin 10 as output
@@ -29,11 +37,6 @@ def play_note(period=0.1, duration=500):
 def sleepm(millis):
 	time.sleep(millis/1000.0)
 
-speed = 120.0
-minim = 2.0 * speed
-crotchet = 1.0 * speed
-quaver = 0.5 * speed
-semiquaver = 0.25 * speed
 
 frequencies = {
 	"C3": 130.81,
@@ -89,14 +92,50 @@ frequencies = {
 	"B5": 987.77
 }
 
+def transpose(pitchstring, semitones):
+    pitch_table = {
+            "C": 0,
+            "C#": 1,
+            "Db": 1,
+            "D": 2,
+            "D#": 3,
+            "Eb": 3,
+            "E": 4,
+            "F": 5,
+            "F#": 6,
+            "Gb": 6,
+            "G": 7,
+            "G#": 8,
+            "Ab": 8,
+            "A": 9,
+            "A#": 10,
+            "Bb": 10,
+            "B": 11
+        }
+    reverse_pitchtable = dict(zip(pitch_table.values(), pitch_table.keys()))
+
+    octave = int(pitchstring[-1])
+    note = pitchstring[:-1]
+    index = pitch_table[note]
+
+    octave += (index+semitones)/12
+    new_index = (index+semitones) % 12
+
+    new_pitchstring = reverse_pitchtable[new_index] + str(octave)
+    return new_pitchstring
+
+
 pitches = {}
 for note, freq in frequencies.iteritems():
-	pitches[note] = 1.0/freq
+    pitches[note] = 1.0/freq
 
 def p(note, duration):
-	play, sleep = duration * 0.75, duration * 0.25
-	play_note(pitches[note], play)
-	sleepm(sleep)
+    if TRANSPOSE_AMOUNT != 0:
+        note = transpose(note, TRANSPOSE_AMOUNT)
+
+    play, sleep = duration * 0.75, duration * 0.25
+    play_note(pitches[note], play)
+    sleepm(sleep)
 
 def scale():
 	for note in ["C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4"]:
@@ -197,5 +236,7 @@ def ohwhenthesaints():
 
 	p("C4", minim * 2)
 
-#imperialmarch()
-scale()
+if __name__ == "__main__":
+    #imperialmarch()
+    #scale()
+    ohwhenthesaints()
